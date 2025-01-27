5a2f72c31b4de7852f20f36868a0ad2acd3a31e4
wojciechderwisz@gmail.com <wojciechderwisz@gmail.com>
2025-01-27 18:55:50 +0100

    add demo lines and upload first dll

--- demo_lines/Unit1.dfm
+++ demo_lines/Unit1.dfm
@@ -1,7 +1,7 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
+  Caption = 'demo lines'
   ClientHeight = 551
   ClientWidth = 762
   Color = clBtnFace
@@ -10,5 +10,15 @@ object Form1: TForm1
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
   TextHeight = 15
+  object Panel1: TPanel
+    Left = 8
+    Top = 8
+    Width = 753
+    Height = 537
+    Caption = 'Panel1'
+    TabOrder = 0
+  end
 end
--- demo_lines/Unit1.pas
+++ demo_lines/Unit1.pas
@@ -3,15 +3,30 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, System.SysUtils, System.Classes, Vcl.Forms, Vcl.Controls, Vcl.StdCtrls,
+  Vcl.ExtCtrls;
+
+type
+  TDXW_InitWindow = function(hWnd: HWND): Integer; stdcall;
+  TDXW_SetTargetWindow = procedure(TargetWindow: Integer); stdcall;
+  TDXW_DemoLines = procedure(LinesCount: Integer); stdcall;
+  TDXW_ReleaseDxwResources = procedure; stdcall;
 
 type
   TForm1 = class(TForm)
+    Panel1: TPanel;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
-    { Private declarations }
+    FDLLHandle: HMODULE;
+    FDXW_InitWindow: TDXW_InitWindow;
+    FDXW_SetTargetWindow: TDXW_SetTargetWindow;
+    FDXW_DemoLines: TDXW_DemoLines;
+    FDXW_ReleaseDxwResources: TDXW_ReleaseDxwResources;
+    FDXWWindowID: Integer;
+    function LoadDll: Boolean;
+    function LoadFunctions: Boolean;
   public
-    { Public declarations }
   end;
 
 var
@@ -20,5 +35,64 @@ var
 implementation
 
 {$R *.dfm}
+
+function TForm1.LoadDll: Boolean;
+begin
+  Result := False;
+  FDLLHandle := LoadLibrary('dxw.dll');
+
+  if FDLLHandle = 0 then
+  begin
+    MessageBox(Handle, 'Failed to load dxw.dll', 'Error', MB_ICONERROR or MB_OK);
+    Exit;
+  end;
+
+  Result := True;
+end;
+
+function TForm1.LoadFunctions: Boolean;
+begin
+  Result := False;
+
+  FDXW_InitWindow := GetProcAddress(FDLLHandle, 'DXW_InitWindow');
+  FDXW_SetTargetWindow := GetProcAddress(FDLLHandle, 'DXW_SetTargetWindow');
+  FDXW_DemoLines := GetProcAddress(FDLLHandle, 'DXW_DemoLines');
+  FDXW_ReleaseDxwResources := GetProcAddress(FDLLHandle, 'DXW_ReleaseDxwResources');
+
+  if not Assigned(FDXW_InitWindow) or
+     not Assigned(FDXW_SetTargetWindow) or
+     not Assigned(FDXW_DemoLines) or
+     not Assigned(FDXW_ReleaseDxwResources) then
+  begin
+    MessageBox(Handle, 'Failed to load one or more functions from dxw.dll', 'Error', MB_ICONERROR or MB_OK);
+    FreeLibrary(FDLLHandle);
+    Exit;
+  end;
+
+  Result := True;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  if not LoadDll then
+    Exit;
+
+  if not LoadFunctions then
+    Exit;
+
+  FDXWWindowID := FDXW_InitWindow(Panel1.Handle);
+
+  FDXW_SetTargetWindow(FDXWWindowID);
+  FDXW_DemoLines(1000000);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+//  if Assigned(FDXW_ReleaseDxwResources) then
+//    FDXW_ReleaseDxwResources;
+
+  if FDLLHandle <> 0 then
+    FreeLibrary(FDLLHandle);
+end;
 
 end.
