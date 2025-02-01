b04e9a986ec695f6738c80a70d9d8d86a02c65b8
wojciechderwisz@gmail.com <wojciechderwisz@gmail.com>
2025-02-01 17:19:08 +0100

    move dll to common lib

--- demo_lines/Unit1.pas
+++ demo_lines/Unit1.pas
@@ -7,6 +7,7 @@ uses
   Vcl.ExtCtrls;
 
 type
+// definicje typów wskaŸników do funkcji z DLL
   TDXW_InitWindow = function(hWnd: HWND): Integer; stdcall;
   TDXW_SetTargetWindow = procedure(TargetWindow: Integer); stdcall;
   TDXW_DemoLines = procedure(LinesCount: Integer); stdcall;
@@ -18,7 +19,9 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
+    // uchwyt do DLL
     DLLHandle: HMODULE;
+    // wskaŸniki do funkcji
     DXW_InitWindow: TDXW_InitWindow;
     DXW_SetTargetWindow: TDXW_SetTargetWindow;
     DXW_DemoLines: TDXW_DemoLines;
@@ -36,10 +39,12 @@ implementation
 
 {$R *.dfm}
 
+
+// ³adowanie DLL
 function TForm1.LoadDll: Boolean;
 begin
   Result := False;
-  DLLHandle := LoadLibrary('dxw.dll');
+  DLLHandle := LoadLibrary('../../../lib/dxw.dll');
 
   if DLLHandle = 0 then
   begin
@@ -50,14 +55,16 @@ begin
   Result := True;
 end;
 
+
+// ³adowanie funkcji z dll
 function TForm1.LoadFunctions: Boolean;
 begin
   Result := False;
 
-  DXW_InitWindow := GetProcAddress(DLLHandle, 'DXW_InitWindow');
-  DXW_SetTargetWindow := GetProcAddress(DLLHandle, 'DXW_SetTargetWindow');
-  DXW_DemoLines := GetProcAddress(DLLHandle, 'DXW_DemoLines');
-  DXW_ReleaseDxwResources := GetProcAddress(DLLHandle, 'DXW_ReleaseDxwResources');
+  DXW_InitWindow := GetProcAddress(DLLHandle, 'DXW_InitWindow');                    // inicjalizacja okna i uzyskanie ID
+  DXW_SetTargetWindow := GetProcAddress(DLLHandle, 'DXW_SetTargetWindow');          // ustawianie aktualnego okna po ID
+  DXW_DemoLines := GetProcAddress(DLLHandle, 'DXW_DemoLines');                      // demo start
+  DXW_ReleaseDxwResources := GetProcAddress(DLLHandle, 'DXW_ReleaseDxwResources');  // zwalnianie zasobów biblioteki
 
   if not Assigned(DXW_InitWindow) or
      not Assigned(DXW_SetTargetWindow) or
@@ -80,9 +87,14 @@ begin
   if not LoadFunctions then
     Exit;
 
+  { inicjalizacja zasobów directx dla Panel1.
+    Biblioteka generuje WindowID które zapisujemy }
   DXWWindowID := DXW_InitWindow(Panel1.Handle);
 
+  // od tego momentu wszystkie dalsze wywo³ania funkcji DXW dotycz¹ okna na Panel1
   DXW_SetTargetWindow(DXWWindowID);
+
+  // start demo
   DXW_DemoLines(1000000);
 end;
 
